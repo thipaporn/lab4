7c32b1e35907d699b059af6113e43555c6a7ede6
thipaporn <35359046+thipaporn@users.noreply.github.com>
2018-01-16 16:36:08 +0700

    600610734

--- lab4_4.cpp
+++ lab4_4.cpp
@@ -28,6 +28,10 @@ int main()
 	int count = 0;
 	while(count < 10){
 		//Edit code here using printString1() and printString2()
+		printString1("5",9-count);
+		printString2("6","9",count);
+		cout <<"\n";
+	
 		
 		count++;
 	}
